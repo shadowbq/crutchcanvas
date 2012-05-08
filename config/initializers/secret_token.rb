bcdf22a026786e161811eb77278e60f8fa533ab4
Shadowbq <shadowbq@gmail.com>
2012-05-08 06:05:16 -0400

    initial commit (not tests)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Crutchcanvas::Application.config.secret_token = 'cb7d93fd4f890e0c8f63144fdf141365145bfc0c177bf801b62d6da0dd44cd57c820e8603dcdffce89b40965cabb5212a9dc53103780f797f75163d4dcb4f974'
